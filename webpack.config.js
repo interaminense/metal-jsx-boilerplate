759c652ce49d78dc45567afc342bc8f5d7b658dd
Adriano Interaminense <adriano.interaminense@liferay.com>
2018-05-05 22:52:48 -0300

    Adds support to scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,21 @@ module.exports = {
 	module: {
 			rules: [
 					{
-							test: /\.(js)$/,
-							exclude: /node_modules/,
-							use: {
-									loader: "babel-loader"
-							}
+						test: /\.(js)$/,
+						exclude: /node_modules/,
+						use: {
+							loader: "babel-loader"
+						}
+					},
+					{
+						test: /\.scss$/,
+						use: [{
+							loader: "style-loader"
+						}, {
+							loader: "css-loader"
+						}, {
+							loader: "sass-loader"
+						}]
 					}
 			]
 	}
